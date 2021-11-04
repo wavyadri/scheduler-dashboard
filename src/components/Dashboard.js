197216bf05d45842f65b326d15e0d08f531907bf
Adriana Calvo-Matos <adriana.calvomatos@gmail.com>
2021-11-04 12:58:50 -0400

    add focused state to each panel

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -29,20 +29,27 @@ const data = [
 ];
 
 class Dashboard extends Component {
-  state = { loading: false };
+  state = { loading: false, focused: null };
 
   render() {
-    const dashboardClasses = classnames('dashboard');
-    const panels = data.map((panel) => {
-      return (
-        <Panel
-          key={panel.id}
-          id={panel.id}
-          label={panel.label}
-          value={panel.value}
-        />
-      );
+    const dashboardClasses = classnames('dashboard', {
+      'dashboard--focused': this.state.focused,
     });
+    const panels = data
+      .filter(
+        (panel) =>
+          this.state.focused === null || this.state.focused === panel.id
+      )
+      .map((panel) => {
+        return (
+          <Panel
+            key={panel.id}
+            id={panel.id}
+            label={panel.label}
+            value={panel.value}
+          />
+        );
+      });
 
     return (
       <>
