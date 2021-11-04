7d3aa0a6a0fc10a01bed2accda796291c037316a
Adriana Calvo-Matos <adriana.calvomatos@gmail.com>
2021-11-04 12:50:33 -0400

    create panels with mock data

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -3,18 +3,53 @@ import React, { Component } from 'react';
 import classnames from 'classnames';
 
 import Loading from './Loading';
+import Panel from './Panel';
+
+const data = [
+  {
+    id: 1,
+    label: 'Total Interviews',
+    value: 6,
+  },
+  {
+    id: 2,
+    label: 'Least Popular Time Slot',
+    value: '1pm',
+  },
+  {
+    id: 3,
+    label: 'Most Popular Day',
+    value: 'Wednesday',
+  },
+  {
+    id: 4,
+    label: 'Interviews Per Day',
+    value: '2.3',
+  },
+];
 
 class Dashboard extends Component {
   state = { loading: false };
+
   render() {
     const dashboardClasses = classnames('dashboard');
+    const panels = data.map((panel) => {
+      return (
+        <Panel
+          key={panel.id}
+          id={panel.id}
+          label={panel.label}
+          value={panel.value}
+        />
+      );
+    });
 
     return (
       <>
         {this.state.loading ? (
           <Loading />
         ) : (
-          <main className={dashboardClasses} />
+          <main className={dashboardClasses}>{panels}</main>
         )}
       </>
     );
